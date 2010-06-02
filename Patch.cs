65fe56243cd351522b46745b2998f7e66ac44557
David Crowell <dcrowell.mit@gmail.com>
2010-06-02 14:26:49 -0400

    temp.  Shortn Dialog changes

---
+++ Patch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+using Microsoft.Office.Tools.Word.Extensions;
+using System.Windows.Forms;
+
+namespace Soylent
+{
+    public class Patch
+    {
+        public Word.Range original;
+        public List<string> replacements;
+
+        public Patch(Word.Range original, List<string> replacements)
+        {
+            this.original = original;
+            this.replacements = replacements;
+        }
+    }
+
+    public class PatchSelection
+    {
+        public Patch patch;
+        public string selection;
+        public bool isOriginal {
+            get {
+                return (patch.original.Text == selection);
+            }
+        }
+
+        public PatchSelection(Patch patch, string selection)
+        {
+            this.patch = patch;
+            this.selection = selection;
+        }
+    }
+}
---
+++ ShortnView.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms.Integration;
+
+namespace Soylent
+{
+    public class ShortnView : HITView
+    {
+        Button ShortnButton;
+        public ShortnView(string workType, ShortenData data) : base(workType, data)
+        {
+            //Globals.Soylent.soylent.Controls.Add(new System.Windows.Forms.Button());
+            ShortnButton = new Button();
+            ShortnButton.Content = "Shortn";
+            ShortnButton.Name = "Shortn";
+            ShortnButton.Height = 23;
+            ShortnButton.Width = 80;
+            ShortnButton.IsEnabled = false;
+            ShortnButton.Click += new RoutedEventHandler(Shortn_Clicked);
+
+            data.register(this);
+            //ShortnButton.
+            //ShortnButton.Click = Shortn_Clicked;
+            stages.Children.Add(ShortnButton);
+        }
+        public void Shortn_Clicked(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.Form newForm = new System.Windows.Forms.Form();
+            newForm.Width = 1195;
+            newForm.Height = 380;
+            newForm.BackColor = System.Drawing.Color.White;
+
+            // Create the ElementHost control for hosting the
+            // WPF UserControl.
+            ElementHost host = new ElementHost();
+            host.Width = newForm.Width;
+            host.Height = newForm.Height;
+
+            // Create the WPF UserControl.
+            //Word.Range toShorten = Globals.Soylent.Application.Selection.Range;
+            ShortenDialog sd = new ShortenDialog(data as ShortenData);
+
+            // Assign the WPF UserControl to the ElementHost control's
+            // Child property.
+
+            host.Child = sd;
+
+            newForm.Visible = false;
+            // Add the ElementHost control to the form's
+            // collection of child controls.
+            newForm.Controls.Add(host);
+            newForm.Show();
+
+            double beforeHeight = sd.before.DesiredSize.Height;
+            newForm.Height = (int)(beforeHeight + 60);
+            sd.lengthSlider.Height = (int)(beforeHeight - 20);
+
+            newForm.Visible = true;
+
+            foreach (StageView stageview in stageList.Values)
+            {
+                stageview.hitProgress.Foreground = Brushes.LightSkyBlue;
+            }
+        }
+        public void shortenDataReceived()
+        {
+            foreach (StageView stageview in stageList.Values)
+            {
+                stageview.hitProgress.Foreground = Brushes.Blue;
+            }
+            this.ShortnButton.IsEnabled = true;
+        }
+    }
+}
